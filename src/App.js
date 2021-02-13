da7485c506da536148c0bbfbeb1a8780e48d03b9
sammiearchie <chinonsosamuel29@gmail.com>
2021-02-13 15:16:46 +0100

    Today app included

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-// import React and Components
-
+import React, {Component} from 'react';
+import Today from './Today/Today';
+import History from './History/History';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <div className="topheader">
+          <header className="container">
+            <nav className="navbar">
+              <div className="navbar-brand">
+                <span className="navbar-item">Sam-Coins</span>
+              </div>
+              <div className="navbar-end">
+                <a className="navbar-item" href="https://sam-coins.com" target="_blank" rel="noopener noreferrer">Sam-coin.com</a>
+              </div>
+            </nav>
+          </header>
+        </div>
+
+        <section className="results--section">
+          <div className="container">
+            <h1>Sam-Coins is a realtime price information about <br /> BTC, ETH, and LTC.</h1>
+          </div>
+          <div className="results--section__inner">
+            <Today />
+            <History />
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default App;
